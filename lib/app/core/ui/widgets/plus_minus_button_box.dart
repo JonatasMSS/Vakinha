b1fe18c2fcea84f2c15b8a7cd82a6f0ca3e54ba4
Jonatas_PCSENAI <jonatas.miguelss@gmail.com>
2022-02-10 16:51:43 -0300

    4 dia terminado

---
+++ lib/app/core/ui/widgets/plus_minus_button_box.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/formatter_helper.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_rounded_button.dart';
+
+class PlusMinusButtonBox extends StatelessWidget {
+  final bool elevated;
+  final Color? backgroundColor;
+  final String? label;
+  final int quantity;
+  final double price;
+  final VoidCallback minusCall;
+  final VoidCallback plussCall;
+  final bool AllCalc;
+
+  const PlusMinusButtonBox({
+    Key? key,
+    required this.quantity,
+    this.elevated = false,
+    this.backgroundColor,
+    this.label,
+    required this.price,
+    required this.minusCall,
+    required this.plussCall,
+    this.AllCalc = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: elevated ? 5 : 0,
+      borderRadius: BorderRadius.circular(20),
+      shadowColor: Colors.black26,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            //Oculta o texto caso ele seja nulo
+            Visibility(
+              visible: label != null,
+              child: Text(
+                label ?? '',
+                style: TextStyle(fontSize: 15),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Row(
+              children: [
+                VakinhaRoundedButton(
+                  onPressed: minusCall,
+                  label: '-',
+                ),
+                Text('$quantity'),
+                VakinhaRoundedButton(
+                  label: '+',
+                  onPressed: plussCall,
+                ),
+              ],
+            ),
+            Visibility(
+              child: const Spacer(),
+              visible: label == null,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 10),
+              constraints: const BoxConstraints(minWidth: 70),
+              child: Text(
+                FormatterHelper.formatCurrency(
+                  AllCalc ? price * quantity : price,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/modules/product_detail/product_detail_page.dart
@@ -0,0 +1,99 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/plus_minus_button_box.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
+import './product_detail_controller.dart';
+
+class ProductDetailPage extends GetView<ProductDetailController> {
+  const ProductDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: VakinhaAppbar(),
+      body: LayoutBuilder(
+        builder: (_, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: context.width,
+                    height: context.heightTransformer(reducedBy: 60),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'http://dartweek.academiadoflutter.com.br/images${controller.product.image}'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(
+                      controller.product.name,
+                      style: context.textTheme.headline4!.copyWith(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      controller.product.description,
+                      style: context.textTheme.bodyText2!.copyWith(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Obx(() {
+                    return PlusMinusButtonBox(
+                      minusCall: controller.removeProduct,
+                      plussCall: controller.addProduct,
+                      price: controller.product.price,
+                      quantity: controller.quantity,
+                    );
+                  }),
+                  const Divider(),
+                  Obx(() {
+                    return ListTile(
+                      title: const Text(
+                        'Total',
+                        style: VakinhaUI.textBold,
+                      ),
+                      trailing: Text(
+                        r'R$ ' + '${controller.totalPrice}',
+                        style: VakinhaUI.textBold,
+                      ),
+                    );
+                  }),
+                  Center(
+                    child: SizedBox(
+                      width: context.widthTransformer(reducedBy: 10),
+                      child:
+                          VakinhaButton(label: 'ADICIONAR', onPressed: () {}),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
